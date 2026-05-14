b07056b356dc1d77335e9a76314a30bb22a14bba
Luis San Martin <luis17.sanmartin@gmail.com>
2026-05-13 23:44:00 -0400

    Delete message fixed

--- models/Task/TaskController.js
+++ models/Task/TaskController.js
@@ -71,6 +71,6 @@ export class TaskController {
     if (!task) {
       return res.status(500).json({ error: "Failed to delete task" });
     }
-    return res.status(200).json(task);
+    return res.status(200).json({ message: "Task deleted successfully" });
   };
 }
